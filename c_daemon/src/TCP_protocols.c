90c38ce8cc016333c63ebd35acbd653c62f40242
dini <dininurdini1011@gmail.com>
2023-03-28 15:29:57 +0200

    add gethostIp function

--- c_daemon/src/TCP_protocols.c
+++ c_daemon/src/TCP_protocols.c
@@ -72,3 +72,27 @@ int create_master_socket(char *myIP) {
 
     return 0;
 }
+
+char * gethostIP() {
+    
+    char hostbuffer[256];
+    char *hostIP;
+    struct hostent *host_entry;
+
+    // To retrieve hostname
+    if((gethostname(hostbuffer, sizeof(hostbuffer))) == -1) {
+        stop("gethostname");
+    } 
+    
+    // To retrieve host information
+    if((host_entry = gethostbyname(hostbuffer)) == -1) {
+        stop("gethostbyname");
+    }
+
+    // To convert an Internet network
+    // address into ASCII string
+    hostIP = inet_ntoa(*((struct in_addr*)host_entry->h_addr_list[0]));
+ 
+    printf("Host IP: %s\n", hostIP);
+    hostIP[strlen(hostIP)] = '\0';
+}

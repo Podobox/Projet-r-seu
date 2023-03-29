70170578fb9ce8ff68643ad95128a57f6b6ba6b1
phuoc <doan_phuoc.le@insa-cvl.fr>
2023-03-29 20:44:53 +0200

    Modifier ipc

--- Controller/Communication.py
+++ Controller/Communication.py
@@ -4,11 +4,12 @@ import sysv_ipc
 import re
 import pickle
 import struct
+import pygame
 from enum import Enum
-# from Model.Player import Player
+#from Model.Player import Player
 
 KEY = 1234
-PY_TO_C = 2
+PY_TO_C = 3
 
 class MessageType(Enum):
     REQUIRE_OWNERSHIP = 1
@@ -28,12 +29,12 @@ class MessageType(Enum):
     REQUIRE_POPULATION_OWNERSHIP = 15
 
 
-class Message(Enum):
-    type: MessageType  # int
-    posx: int  # llu
-    posy: int  # llu
-    playerID: int  # llu (or building depending on the message)
-    add: int  # llu (additional field, walker_type)
+# class Message(Enum):
+#     type: MessageType  # int
+#     posx: int  # llu
+#     posy: int  # llu
+#     playerID: int  # llu (or building depending on the message)
+#     add: int  # llu (additional field, walker_type)
 
 
 class Communication:
@@ -43,55 +44,56 @@ class Communication:
         self.message = None
 
     def send_message_from_py_to_c(self, message):
-        self.message_queue.send(message.encode(), type=PY_TO_C)
+        # send the actions from python to c
+        self.message_queue.send(message, type=PY_TO_C)
 
     def ask_for_money_ownership(self):
         message = struct.pack("iQQQQ", MessageType.REQUIRE_MONEY_OWNERSHIP, 0, 0, 0, 0)
-        self.send_all(message)
+        self.send_message_from_py_to_c(message)
 
     def ask_for_population_ownership(self):
         message = struct.pack("iQQQQ", MessageType.REQUIRE_POPULATION_OWNERSHIP, 0, 0, 0, 0)
-        self.send_all(message)
+        self.send_message_from_py_to_c(message)
 
     def build(self, posx, posy, type):
         message = struct.pack("iQQQQ", MessageType.BUILD, posx, posy, type, 0)
-        self.send_all(message)
+        self.send_message_from_py_to_c(message)
 
     def destroy(self, posx, posy):
-        message = struct.pack("iQQQQ", MessageType.DESTROY, posx, posy, 0, 0)
-        self.send_all(message)
+        message = struct.pack("iQQQQ", MessageType.DESTROY.value, posx, posy, 0, 0)
+        self.send_message_from_py_to_c(message)
 
     def catch_fire(self, posx, posy):
-        message = struct.pack("iQQQQ", MessageType.CATCH_FIRE, posx, posy, 0, 0)
-        self.send_all(message)
+        message = struct.pack("iQQQQ", MessageType.CATCH_FIRE.value, posx, posy, 0, 0)
+        self.send_message_from_py_to_c(message)
 
     def put_out_fire(self, posx, posy):
-        message = struct.pack("iQQQQ", MessageType.PUT_OUT_FIRE, posx, posy, 0, 0)
-        self.send_all(message)
+        message = struct.pack("iQQQQ", MessageType.PUT_OUT_FIRE.value, posx, posy, 0, 0)
+        self.send_message_from_py_to_c(message)
 
     def evolve(self, posx, posy):
-        message = struct.pack("iQQQQ", MessageType.EVOLVE, posx, posy, 0, 0)
-        self.send_all(message)
+        message = struct.pack("iQQQQ", MessageType.EVOLVE.value, posx, posy, 0, 0)
+        self.send_message_from_py_to_c(message)
 
     def devolve(self, posx, posy):
-        message = struct.pack("iQQQQ", MessageType.DEVOLVE, posx, posy, 0, 0)
-        self.send_all(message)
+        message = struct.pack("iQQQQ", MessageType.DEVOLVE.value, posx, posy, 0, 0)
+        self.send_message_from_py_to_c(message)
 
     def move_walker(self, posx, posy, building, walker_type):
-        message = struct.pack("iQQQQ", MessageType.MOVE_WALKER, posx, posy, building,
+        message = struct.pack("iQQQQ", MessageType.MOVE_WALKER.value, posx, posy, building,
                               walker_type)
-        self.send_all(message)
+        self.send_message_from_py_to_c(message)
 
     def ask_for_ownership(self, posx, posy, player, me):
         message = struct.pack("iQQQQ", MessageType.REQUIRE_OWNERSHIP, posx, posy, player, 0)
-        self.send(message)
+        self.send_message_from_py_to_c(message)
         # wait for answer and return
         message = struct.pack("iQQQQ", MessageType.CHANGE_OWNERSHIP, posx, posy, me, 0)
-        self.send_all(message)
+        self.send_message_from_py_to_c(message)
 
     def give_ownership(self, posx, posy):
-        message = struct.pack("iQQQQ", MessageType.GIVE_OWNERSHIP, posx, posy, 0, 0)
-        self.send(message)
+        message = struct.pack("iQQQQ", MessageType.GIVE_OWNERSHIP.value, posx, posy, 0, 0)
+        self.send_message_from_py_to_c(message)
 
     def check_messages(self):
         # check for messages from other players
@@ -100,15 +102,15 @@ class Communication:
         pass
 
     def connect(self, ip, port):
-        message = struct.pack("iQQQQ", MessageType.CONNECT, ip, port, 0, 0)
-        self.send(message)
+        message = struct.pack("iQQQQ", MessageType.CONNECT.value, ip, port, 0, 0)
+        self.send_message_from_py_to_c(message)
 
         # return the game it is connected to and its players
         pass
 
     def diconnect(self):
-        message = struct.pack("iQQQQ", MessageType.DISCONNECT, 0, 0, 0, 0)
-        self.send_all(message)
+        message = struct.pack("iQQQQ", MessageType.DISCONNECT.value, 0, 0, 0, 0)
+        self.send_message_from_py_to_c(message)
         # cleanly disconnectand release all owned objects
         # if done well I believe the process is the same wether there are players still
         # connected or not
@@ -120,9 +122,14 @@ class Communication:
         # probably not the best file for this function
         pass
 
-while (True):
-    Sender = Communication()
-    string = input("")
-    Sender.send_message_from_py_to_c(string)
-    if (str(string) == 'end' or str(string) == 'exit' or str(string) == ''):
-        break
+Sender = Communication() 
+Sender.evolve(9, 5) #11
+Sender.devolve(9, 6) #12
+Sender.diconnect() #4
+Sender.connect(3, 8000) #3
+Sender.give_ownership(9, 7) #2
+Sender.move_walker(9, 8, 2, 3) #13
+Sender.put_out_fire(10, 9)
+
+# if (str(string) == 'end' or str(string) == 'exit' or str(string) == ''):
+#     break
--- Controller/receive_msg_from_python.c
+++ Controller/receive_msg_from_python.c
@@ -4,11 +4,12 @@
 #include <string.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 #define BUF_SIZE 1024
 #define MAX 10
 #define MESG_KEY 1234
-#define PY_TO_C 2
+#define PY_TO_C 3
 #define False 0
 #define True 1
 
@@ -18,6 +19,17 @@ struct mesg_buffer
     long mesg_type;
     char mesg_text[100];
 } message;
+
+struct python_struct_t {
+    int32_t message_type;
+    uint64_t posx;
+    uint64_t posy;
+    uint64_t type;
+    uint64_t x;
+};
+
+struct python_struct_t *buffer;
+
 
 void recv_from_python()
 {
@@ -35,13 +47,17 @@ void recv_from_python()
     }
     // display the message
     message.mesg_text[length] = '\0';
+    buffer = (struct python_struct_t *) message.mesg_text;
 }
 
 int main()
 {
+    buffer = calloc(sizeof(struct python_struct_t), 1);
+
     while (True)
     {
         recv_from_python();
-        printf("Data send is : %s \n", message.mesg_text);
+        //printf("Data send is : %s \n", message.mesg_text);
+        printf("Sending data to C : %d %llu %llu %llu %llu \n", buffer->message_type, buffer->posx, buffer->posy, buffer->type, buffer->x);
     }
 }
